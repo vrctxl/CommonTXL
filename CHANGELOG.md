13711bc4ed20bf408792a5157e24efaf4c06f356
Justin Aquadro <jaquadro@gmail.com>
2023-08-31 19:19:15 -0400

    Add AccessControlGraphAPI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.0.2] - 08-31-23
+
+- Add Runtime/Scripts/AccessControlGraphAPI to expose a usable Access Control API to Udon Graph
+
 ## [1.0.1] - 08-06-23
 
 - Add _AddLocalPlayer and _RemoveLocalPlayer methods to ZoneMembership
---
+++ Packages/com.texelsaur.common/Runtime/Scripts/AccessControlGraphAPI.cs
@@ -0,0 +1,71 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Texel
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class AccessControlGraphAPI : UdonSharpBehaviour
+    {
+        [HideInInspector] public AccessControlUserSource aclUserSourceArg;
+        [HideInInspector] public VRCPlayerApi whitelistPlayerArg;
+        [HideInInspector] public VRCPlayerApi accessPlayerArg;
+        [HideInInspector] public Component componentArg;
+        [HideInInspector] public bool enforceArg;
+
+        [HideInInspector] public bool playerWhitelistedReturn;
+        [HideInInspector] public bool localWhitelistedReturn;
+        [HideInInspector] public bool playerHasAccessReturn;
+        [HideInInspector] public bool localHasAccessReturn;
+
+        public AccessControl accessControl;
+
+        void Start()
+        {
+            if (!accessControl)
+                accessControl = GetComponentInParent<AccessControl>();
+        }
+
+        public void _AddUserSource()
+        {
+            accessControl._AddUserSource(aclUserSourceArg);
+        }
+
+        public void _Enforce()
+        {
+            accessControl._Enforce(enforceArg);
+        }
+
+        public void _RefreshWhitelistCheck()
+        {
+            accessControl._RefreshWhitelistCheck();
+        }
+
+        public void _PlayerWhitelisted()
+        {
+            playerWhitelistedReturn = accessControl._PlayerWhitelisted(whitelistPlayerArg);
+        }
+
+        public void _LocalWhitelisted()
+        {
+            localWhitelistedReturn = accessControl._LocalWhitelisted();
+        }
+
+        public void _HasAccess(VRCPlayerApi player)
+        {
+            playerHasAccessReturn = accessControl._HasAccess(accessPlayerArg);
+        }
+
+        public void _LocalHasAccess()
+        {
+            localHasAccessReturn = accessControl._LocalHasAccess();
+        }
+
+        public void _Validate()
+        {
+            accessControl._Validate();
+        }
+    }
+}
